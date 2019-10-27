65a0f413c2ca050cf1fee0e789abbd4af406db2e
luoyang <10955717@qq.com>
2019-10-27 18:46:40 +0800

    reset multi app config.

--- app/middleware.php
+++ app/middleware.php
@@ -2,5 +2,6 @@
 return [
     think\middleware\CheckRequestCache::class,
     think\middleware\LoadLangPack::class,
-    think\middleware\SessionInit::class
+    think\middleware\SessionInit::class,
+    app\middleware\Auth::class
 ];
--- app/middleware/Auth.php
+++ app/middleware/Auth.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace app\http\middleware;
+namespace app\middleware;
 
 class Auth
 {
     public function handle($request, \Closure $next)
     {
         $uid = session('user.id');
-        $path = $request->root() . '/' . $request->path();
+        $path = $request->root() . '/' . $request->pathinfo();
         return $next($request);
     }
 }
